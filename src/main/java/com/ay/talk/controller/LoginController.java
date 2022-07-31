266e29aa8e5c60b626f7aa2df1b100d6c1550182
ksiisk99@gmail.com <ksiisk99@gmail.com>
2022-07-31 22:11:24 +0900

    ServerService클래스 삭제로 인한 @EventListener삭제

--- src/main/java/com/ay/talk/controller/LoginController.java
+++ src/main/java/com/ay/talk/controller/LoginController.java
@@ -2,6 +2,8 @@ package com.ay.talk.controller;
 
 import java.io.IOException;
 
+import javax.annotation.PostConstruct;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.hibernate.internal.build.AllowSysOut;
@@ -9,12 +11,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.context.event.EventListener;
-import org.springframework.data.redis.core.ListOperations;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,13 +21,10 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ay.talk.dto.response.ResLogin;
 import com.ay.talk.dto.response.ResLogout;
 import com.ay.talk.dto.response.ResPcLogin;
-import com.ay.talk.jpaentity.Member;
-import com.ay.talk.jparepository.MemberRepository;
 import com.ay.talk.dto.request.ReqLogin;
 import com.ay.talk.dto.request.ReqLogout;
 import com.ay.talk.dto.request.ReqPcLogin;
 import com.ay.talk.service.LoginService;
-import com.ay.talk.service.ServerService;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.LoggerContext;
@@ -47,31 +40,24 @@ import io.swagger.annotations.ApiOperation;
 @RequestMapping("/ay")
 @PropertySource("classpath:application.properties")
 public class LoginController{
-	//private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(LoginController.class);
 	LoggerContext loggerContext=(LoggerContext) LoggerFactory.getILoggerFactory();
 	ch.qos.logback.classic.Logger mongoLogger=loggerContext.getLogger("org.mongodb.driver");
 	ch.qos.logback.classic.Logger redisLogger=loggerContext.getLogger("io.lettuce.core");
-	private final ServerService serverService;
 	private final LoginService loginService;
 	private Logger logger;
 	
 	@Autowired
-	public LoginController(ServerService serverService, LoginService loginService) {
-		this.serverService=serverService;
+	public LoginController(LoginService loginService) {
 		this.loginService=loginService;
 		logger=LogManager.getLogger();
 	}
 	
-	@EventListener(ContextRefreshedEvent.class)
-	public void ServerSetting() throws IOException {
-		//몽고디비와 레디스 수많은 로그발생을 막음
+	@PostConstruct
+	public void init() {
 		mongoLogger.setLevel(Level.OFF);
 		redisLogger.setLevel(Level.OFF);
-		//fcmService.Init(); //fcm세팅
-		serverService.init(); //서버 인 메모리 초기화
-		logger.info("server start");
 	}
-	
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 * @throws FirebaseMessagingException 
