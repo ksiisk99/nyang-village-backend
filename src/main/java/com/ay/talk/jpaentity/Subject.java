a8a1a0881724a831e506f8db34da3c2741ffa6ff
ksiisk99@gmail.com <ksiisk99@gmail.com>
2022-08-03 00:50:36 +0900

    과목 테이블

---
+++ src/main/java/com/ay/talk/jpaentity/Subject.java
@@ -0,0 +1,31 @@
+package com.ay.talk.jpaentity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Subject {
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "subject_id")
+	private Long Id;
+	@Column(length = 30)
+	private String name;
+	
+	protected Subject() {}
+
+	public Subject(String name) {
+		this.name = name;
+	}
+	
+	public Long getId() {
+		return Id;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+}
