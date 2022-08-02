f15ec649d0537b95a2322bc9d5c619c119e1f9d4
ksiisk99@gmail.com <ksiisk99@gmail.com>
2022-08-03 00:50:54 +0900

    정지 유저 테이블

---
+++ src/main/java/com/ay/talk/jpaentity/Suspended.java
@@ -0,0 +1,46 @@
+package com.ay.talk.jpaentity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Suspended {
+	@Id
+	@Column(name = "student_id", length = 10)
+	private String studentId;
+	@Column(length = 6)
+	private String period;
+	@Column(name = "report_content")
+	private String reportContent;
+	@Column(name = "report_why")
+	private String reportWhy;
+	
+	protected Suspended() {}
+
+	public Suspended(String studentId, String period, String reportContent, String reportWhy) {
+		super();
+		this.studentId = studentId;
+		this.period = period;
+		this.reportContent = reportContent;
+		this.reportWhy = reportWhy;
+	}
+
+	public String getStudentId() {
+		return studentId;
+	}
+
+	public String getPeriod() {
+		return period;
+	}
+
+	public String getReportContent() {
+		return reportContent;
+	}
+
+	public String getReportWhy() {
+		return reportWhy;
+	}
+	
+	
+}
