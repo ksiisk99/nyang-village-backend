127cdc239f27efa92786f9fc2125f167056c4551
ksiisk99@gmail.com <ksiisk99@gmail.com>
2022-08-03 00:49:55 +0900

    채팅메시지 테이블

---
+++ src/main/java/com/ay/talk/jpaentity/ChatMsg.java
@@ -0,0 +1,53 @@
+package com.ay.talk.jpaentity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class ChatMsg {
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "chat_msg_id")
+    private Long Id;
+	@Column(name = "room_id", length = 4)
+	private String roomId;
+	@Column(name = "nick_name", length = 20)
+	private String nickName;
+	private String content;
+	@Column(length = 6)
+	private String time;
+	
+	protected ChatMsg() {}
+
+	public ChatMsg(String roomId, String nickName, String content, String time) {
+		super();
+		this.roomId = roomId;
+		this.nickName = nickName;
+		this.content = content;
+		this.time = time;
+	}
+
+	public Long getId() {
+		return Id;
+	}
+
+	public String getRoomId() {
+		return roomId;
+	}
+
+	public String getNickName() {
+		return nickName;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public String getTime() {
+		return time;
+	}
+	
+	
+}
