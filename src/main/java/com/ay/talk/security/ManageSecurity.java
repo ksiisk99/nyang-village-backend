1b6d6b5567de7f47e0ef6ba35596241fda56c8d7
ksiisk99@gmail.com <ksiisk99@gmail.com>
2022-08-05 01:40:37 +0900

    권한 enum클래스로 변경

--- src/main/java/com/ay/talk/security/ManageSecurity.java
+++ src/main/java/com/ay/talk/security/ManageSecurity.java
@@ -1,25 +1,23 @@
 package com.ay.talk.security;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.ay.talk.jpaentity.Authority;
 import com.ay.talk.jwt.JwtTokenProvider;
 
 @Component
 public class ManageSecurity {
 	JwtTokenProvider jwtTokenProvider;
-	String authority="Manager";
 	
 	@Autowired
 	public ManageSecurity(JwtTokenProvider jwtTokenProvider) {
 		this.jwtTokenProvider=jwtTokenProvider;
 	}
 	public boolean isJwt(String jwt) {
-		List<String> authorities=jwtTokenProvider.getAuthorities(jwt);
-		if(authorities==null)return false;
-		if(authorities.contains(authority))
+		Authority authority=jwtTokenProvider.getAuthority(jwt);
+		if(authority==null)return false;
+		if(authority==Authority.Manager)
 			return true;
 		return false;
 	}
