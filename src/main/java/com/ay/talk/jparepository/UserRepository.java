920eae80a7e61d68bd3bc10e577506242c78dedb
ksiisk99@gmail.com <ksiisk99@gmail.com>
2022-08-06 17:36:04 +0900

    하위엔티티 SELECT시 N+1문제를 @EntityGraph추가로 해결

--- src/main/java/com/ay/talk/jparepository/UserRepository.java
+++ src/main/java/com/ay/talk/jparepository/UserRepository.java
@@ -1,9 +1,21 @@
 package com.ay.talk.jparepository;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.EntityGraph.EntityGraphType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ay.talk.jpaentity.User;
+import java.lang.String;
+
 
 public interface UserRepository extends JpaRepository<User, String>{
+	@Override
+	@EntityGraph(attributePaths = {"userRoomInfos"}, type = EntityGraphType.FETCH)
+	List<User> findAll();
 	
+	@EntityGraph(attributePaths = {"userRoomInfos"}, type = EntityGraphType.FETCH)
+	Optional<User> findByStudentId(String studentId);
 }
