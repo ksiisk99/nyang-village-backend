0e053f5c90f8ef4cbdac7bd6117dd388ea606760
ksiisk99@gmail.com <ksiisk99@gmail.com>
2022-08-08 16:42:11 +0900

    cors정책 허용

--- src/main/java/com/ay/talk/config/SecurityConfig.java
+++ src/main/java/com/ay/talk/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.ay.talk.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,6 +10,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.ay.talk.jwt.JwtAuthenticationEntryPoint;
 import com.ay.talk.jwt.JwtAuthenticationFilter;
@@ -48,8 +52,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		// TODO Auto-generated method stub
 
 		http
-			.cors().and()
 			.httpBasic().disable()
+			.cors().configurationSource(corsConfigurationSource())
+			.and()
 			.csrf().disable()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 			.and().authorizeHttpRequests()
@@ -62,4 +67,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 					UsernamePasswordAuthenticationFilter.class);
 	}
 	
+	//cors 허용
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration=new CorsConfiguration();
+		configuration.addAllowedOrigin("*"); //URL
+		configuration.addAllowedHeader("*"); //Header
+		configuration.addAllowedMethod("*"); //Method
+		configuration.setAllowCredentials(true);
+		
+		UrlBasedCorsConfigurationSource source=new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
 }
